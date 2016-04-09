202105177e9552e43b5d7477f9d4d21c03612f82
Cong <congusbongus@gmail.com>
2016-04-09 20:41:54 +1000

    Spawner text

--- scripts/spawner.js
+++ scripts/spawner.js
@@ -1,13 +1,17 @@
 var Spawner = function(game, group) {
   this.counts = [0, 0, 0];
   var y = SCREEN_HEIGHT - 10 - 16;
-  group.add(game.make.sprite(
-    SCREEN_WIDTH - 16 * 5 - 10, y, 'outlaw_icon'
-  ));
-  group.add(game.make.sprite(
-    SCREEN_WIDTH - 16 * 3 - 10, y, 'cowboy_icon'
-  ));
-  group.add(game.make.sprite(
-    SCREEN_WIDTH - 16 * 1 - 10, y, 'bandito_icon'
-  ));
+  var style = {
+    font: '16px VT323', fill: '#fff', align: 'right'
+  };
+  var addDisplay = function(icon, ord, counts) {
+    var x = SCREEN_WIDTH - 16 * ((3 - ord) * 2 - 1) - 10;
+    var text = game.add.text(x - 2, y, counts[ord], style);
+    text.anchor.set(1, 0);
+    group.add(text);
+    group.add(game.make.sprite(x, y, icon));
+  }
+  addDisplay('outlaw_icon', 0, this.counts);
+  addDisplay('cowboy_icon', 1, this.counts);
+  addDisplay('bandito_icon', 2, this.counts);
 };
